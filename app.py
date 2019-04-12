64c4f94dc738a711d6760a7f4d609a120702eb5c
alimar05 <alimar05@mail.ru>
2019-04-12 14:09:06 +0300

    without MySQL

--- app.py
+++ app.py
@@ -10,10 +10,18 @@ def log_request(req: 'flask_request', res: str) -> None:
 
 
 @app.route('/viewlog')
-def view_the_log() -> str:
+def view_the_log() -> 'html':
+    contents = []
     with open('vsearch.log') as log:
-        contents = log.read()
-    return escape(contents)
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents)
 
 
 @app.route('/search4', methods=['POST'])
